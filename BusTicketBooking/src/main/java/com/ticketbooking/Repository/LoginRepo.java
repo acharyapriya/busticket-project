7a9cc457277c9dab62441966b861260604f56549
acharyapriya <priyaacharya3012@gmail.com>
2022-06-17 12:33:04 +0530

    deleted completed

--- BusTicketBooking/src/main/java/com/ticketbooking/Repository/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ticketbooking.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ticketbooking.Entity.Login;
-
-public interface LoginRepo extends JpaRepository<Login, String> {
-
-}
--- BusTicketBooking/src/main/java/com/ticketbooking/Repository/TicketBookingRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ticketbooking.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ticketbooking.Entity.TicketBooking;
-
-public interface TicketBookingRepo extends JpaRepository<TicketBooking, String> {
-
-}
