c033fb85b8e74073adf92dffa116ec07feb76595
acharyapriya <priyaacharya3012@gmail.com>
2022-07-27 15:44:47 +0530

    check

--- ticketbooking/src/app/app.component.ts
+++ ticketbooking/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +14,33 @@ export class AppComponent implements OnInit  {
     
   }
   title = 'ticketbooking';
+ 
+visible:boolean=true;
+
+
+
+  constructor(private router:Router , private route: ActivatedRoute,private titleService: Title) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+          let child = this.route.firstChild;
+          while (child) {
+              if (child.firstChild) {
+                  child = child.firstChild;
+              } else if (child.snapshot.data &&    child.snapshot.data['title']) {
+                  return child.snapshot.data['title'];
+              } else {
+                  return null;
+              }
+          }
+          return null;
+      })
+  ).subscribe( (data: any) => {
+      if (data) {
+          this.titleService.setTitle(data + '-busbookink');
+          data.navbar===true ?this.visible=true:this.visible=false; 
+      }
+  });
+
+   }
 }
